9c20c991be0cff1244e2b47ccf090e57f43b8bf8
pythoro <pythoro@mindquip.com>
2019-09-01 21:05:13 +1200

    [ADD] Unittests for filtered and where methods.

--- test/test_box.py
+++ test/test_box.py
@@ -27,3 +27,35 @@ class Test_Box(unittest.TestCase):
         self.assertEqual(len(b), 1)
         expected = {'index': 0, 'a': 1, 'b': 2, 'test': 7}
         self.assertEqual(b[0], expected)
+        
+    def test_filtered(self):
+        b = Box()    
+        dct = get_dct()
+        b.add(dct, 'test', 7)
+        b.add(dct, 'test2', 8)
+        dct['b'] = 3
+        b.add(dct, 'test2', 9)
+        dct['b'] = 6
+        b.add(dct, 'test', 11)
+        expected = [{'index': 1, 'a': 1, 'b': 2, 'test2': 8},
+                    {'index': 2, 'a': 1, 'b': 3, 'test2': 9}]
+        ret = b.filtered('test2')
+        self.assertEqual(ret, expected)
+        ret = b.filtered(['test2'])
+        self.assertEqual(ret, expected)
+        
+    def test_where(self):
+        b = Box()    
+        dct = get_dct()
+        b.add(dct, 'test', 7)
+        b.add(dct, 'test2', 8)
+        dct['b'] = 3
+        b.add(dct, 'test', 9)
+        b.add(dct, 'test2', 11)
+        expected = [{'index': 2, 'a': 1, 'b': 3, 'test': 9},
+                    {'index': 3, 'a': 1, 'b': 3, 'test2': 11}]
+        ret = b.where({'b': 3})
+        self.assertEqual(ret, expected)
+        ret = b.where(b=3)
+        self.assertEqual(ret, expected)
+        
