6ba54fa3bce72dcc1ba7fc4939266ae1e0fec680
pythoro <pythoro@mindquip.com>
2019-09-09 22:09:10 +1200

    [IMP] Improve persist module.

--- resultbox/persist.py
+++ resultbox/persist.py
@@ -5,45 +5,86 @@ Created on Tue Sep  3 21:22:35 2019
 @author: Reuben
 """
 
-from .box import Box
 import json_tricks
-import os
 
-def ensure_ext(fname, ext='.box'):
-    if fname.endswith(ext):
-        return fname
-    else:
-        return fname + ext
+
+class Manager():
+    def __init__(self):
+        self.handlers = {'box': JSON(),
+                         'cbox': CJSON()}
+        self.specified = None
+        
+    def add_handler(self, key, handler):
+        self.handlers[key] = handler
+        
+    def specify(self, key):
+        self.specified = key
+        
+    def load(self, source, handler=None, **kwargs):
+        if handler is not None:
+            return self.handlers[handler].load(source, **kwargs)
+        if self.specified is not None:
+            return self.handlers[self.specified].load(source, **kwargs)
+        for k, handler in self.handlers.items():
+            if handler.suitable(source, **kwargs):
+                return handler.load(source, **kwargs)
+        raise ValueError('No suitable load handler found for source: "'
+                          + source + '"')
+
+    def save(self, box, target, handler=None, **kwargs):
+        if handler is not None:
+            return self.handlers[handler].save(box, target, **kwargs)
+        if self.specified is not None:
+            return self.handlers[self.specified].save(box, target, **kwargs)
+        for k, handler in self.handlers.items():
+            if handler.suitable(target, **kwargs):
+                return handler.save(box, target, **kwargs)
+        raise ValueError('No suitable save handler found for target: "' 
+                         + target + '"')
 
 
 class JSON():
-    def save(self, box, fname, compression=True):
-        mode = 'wb' if compression else 'w'
-        ext = '.cbox' if compression else '.box'
-        with open(ensure_ext(fname, ext), mode=mode) as f:
-            json_tricks.dump(list(box), f, compression=compression)
+    def suitable(self, fname, **kwargs):
+        if fname.endswith('.box'):
+            return True
         
+    def save(self, box, fname, **kwargs):
+        with open(fname, mode='w') as f:
+            json_tricks.dump(list(box), f, compression=False)
     
-    def load(self, fname):
-        compressed =  os.path.isfile(ensure_ext(fname, '.cbox'))
-        ext = '.cbox' if compressed else '.box'
-        mode = 'rb' if compressed else 'r'
-        with open(ensure_ext(fname, ext), mode=mode) as f:
+    def load(self, fname, **kwargs):
+        with open(fname, mode='r') as f:
             lst = json_tricks.load(f)
         # Tidy up from the default OrderedDict of json-tricks,
         # Now dicts are ordered in Python 3.7+.
         for row in lst:
+            row['dependent'] = dict(row['dependent'])
             row['independent'] = dict(row['independent'])
         lst = [dict(row) for row in lst]
-        return Box(lst)
-    
-    
-handler = JSON()
-save = handler.save
-load = handler.load
+        return lst
 
-def set_handler(cls):
-    global handler, save, load
-    handler = cls()
-    save = handler.save
-    load = handler.load
+
+class CJSON():
+    def suitable(self, fname, **kwargs):
+        if fname.endswith('.cbox'):
+            return True
+        
+    def save(self, box, fname, **kwargs):
+        with open(fname, mode='wb') as f:
+            json_tricks.dump(list(box), f, compression=True)
+    
+    def load(self, fname, **kwargs):
+        with open(fname, mode='rb') as f:
+            lst = json_tricks.load(f)
+        # Tidy up from the default OrderedDict of json-tricks,
+        # Now dicts are ordered in Python 3.7+.
+        for row in lst:
+            row['dependent'] = dict(row['dependent'])
+            row['independent'] = dict(row['independent'])
+        lst = [dict(row) for row in lst]
+        return lst
+            
+            
+manager = Manager()
+load = manager.load
+save = manager.save
--- test/test_persist.py
+++ test/test_persist.py
@@ -12,29 +12,29 @@ import os
 from resultbox import Box, load, save
 
 def get_lst():
-    lst = [{'index': 0, 'independent': {'a': 1, 'b': 1, 'c': 1}, 'key': 'd', 'value': 11},
-           {'index': 1, 'independent': {'a': 1, 'b': 2, 'c': 2}, 'key': 'd', 'value': 12},
-           {'index': 2, 'independent': {'a': 1, 'b': 2, 'c': 1}, 'key': 'd', 'value': 13},
-           {'index': 3, 'independent': {'a': 1, 'b': 1, 'c': 2}, 'key': 'd', 'value': 14},
-           {'index': 4, 'independent': {'a': 2, 'b': 1, 'c': 1}, 'key': 'd', 'value': 15},
-           {'index': 5, 'independent': {'a': 2, 'b': 1, 'c': 2}, 'key': 'd', 'value': 16},
-           {'index': 6, 'independent': {'a': 2, 'b': 2, 'c': 1}, 'key': 'd', 'value': 17},
-           {'index': 7, 'independent': {'a': 2, 'b': 2, 'c': 2}, 'key': 'd', 'value': 18}]
+    lst = [{'index': 0, 'independent': {'a': 1, 'b': 1, 'c': 1}, 'dependent': {'d': 11}},
+           {'index': 1, 'independent': {'a': 1, 'b': 2, 'c': 2}, 'dependent': {'d': 12}},
+           {'index': 2, 'independent': {'a': 1, 'b': 2, 'c': 1}, 'dependent': {'d': 13}},
+           {'index': 3, 'independent': {'a': 1, 'b': 1, 'c': 2}, 'dependent': {'d': 14}},
+           {'index': 4, 'independent': {'a': 2, 'b': 1, 'c': 1}, 'dependent': {'d': 15}},
+           {'index': 5, 'independent': {'a': 2, 'b': 1, 'c': 2}, 'dependent': {'d': 16}},
+           {'index': 6, 'independent': {'a': 2, 'b': 2, 'c': 1}, 'dependent': {'d': 17}},
+           {'index': 7, 'independent': {'a': 2, 'b': 2, 'c': 2}, 'dependent': {'d': 18}}]
     return lst
 
 class Test_JSON(unittest.TestCase):
     def test_uncompresed_save_load(self):
         box = Box(get_lst())
         with tempfile.TemporaryDirectory() as tmpdirname:
-            fname = os.path.join(tmpdirname, 'test')
-            save(box, fname, compression=False)
+            fname = os.path.join(tmpdirname, 'test.box')
+            save(box, fname)
             box_check = load(fname)
         self.assertListEqual(box, box_check)
 
     def test_compressed_save_load(self):
         box = Box(get_lst())
         with tempfile.TemporaryDirectory() as tmpdirname:
-            fname = os.path.join(tmpdirname, 'test')
+            fname = os.path.join(tmpdirname, 'test.cbox')
             save(box, fname, compression=True)
             box_check = load(fname)
         self.assertListEqual(box, box_check)
