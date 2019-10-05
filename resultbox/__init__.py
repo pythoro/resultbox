4e476608b9123dc5bef015ceb50ad3a6e150ab91
pythoro <pythoro@mindquip.com>
2019-10-05 17:35:11 +1300

    [IMP] Import all modules to root level.

--- resultbox/__init__.py
+++ resultbox/__init__.py
@@ -5,7 +5,7 @@ Created on Sun Sep  1 14:22:13 2019
 @author: Reuben
 """
 
-from . import utils
+from . import utils, variable, dct, box, table, persist
 from .variable import Store, Variable, Aliases
 from .dct import Dict_Container, get_dict
 from .box import Box
